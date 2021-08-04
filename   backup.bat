4ed313ed3e7b46f0c60fb49b65194a4900ea6824
Jorsh <joshm581@gmail.com>
2021-08-04 16:57:37 -0700

    added new .bat for backups

---
+++   backup.bat
@@ -0,0 +1,10 @@
+:: Script Name:     RoboFolder
+:: Author:          marburgja
+:: Last Rev:        20210804
+:: Purpose:         Creates a backup of files in folder
+
+:: Main
+
+robocopy C:\Users\j.thompson\Desktop\MyProjectFiles E:\Users\j.thompson\Desktop\MyProjectFiles /E
+
+:: End
