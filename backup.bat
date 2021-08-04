f61ae6214588127170ca0cb69fe09000f65eefc7
Joshua Marburg <87879035+marburgja@users.noreply.github.com>
2021-08-04 16:59:40 -0700

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -2,6 +2,7 @@
 :: Author:          marburgja
 :: Last Rev:        20210804
 :: Purpose:         Creates a backup of files in folder
+:: Additional:      Create task in "Task Scheduler" to run it on a schedule
 
 :: Main
 
