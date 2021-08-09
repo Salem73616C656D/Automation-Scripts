882b6a0598a4fcdd99d30e375ae5134f17cfab5f
marburgja <joshm581@gmail.com>
2021-08-09 16:06:26 -0700

    added new endpoint machine script

--- processor.ps1
+++ processor.ps1
@@ -6,31 +6,31 @@
 # Main
 
 # prints/sorts all running processes by CPU usage high>low
-get-process | sort-object cpu -descending
+##get-process | sort-object cpu -descending
 
 # prints/sorts all running processes by PID number high>low
-get-process | sort-object id -descending
+##get-process | sort-object id -descending
 
 # prints/sorts all running processes by Working Set high>low
-get-process | sort-object ws -descending
+##get-process | sort-object ws -descending
 
 # open internet explorer to *website*
-[system.diagnostics.process]::start("iexplore","https://owasp.org/www-project-top-ten/")
+##[system.diagnostics.process]::start("chrome","https://owasp.org/www-project-top-ten/")
 
 # change to "chrome" "msedge" "firefox" for different browsers
-start-job -scriptblock {10 | foreach-object {[system.diagnostics.process]::start("iexplore","https://owasp.org/www-project-top-ten/")}}
+##start-job -scriptblock {10 | foreach-object {[system.diagnostics.process]::start("iexplore","https://owasp.org/www-project-top-ten/")}}
 
 # open multiple internet explorer windows to *website*
-$variable=10
+$variable=20
 for ($x=0;$x -lt $variable;$x++)
 {
-    [system.diagnostics.process]::start("iexplore","https://owasp.org/www-project-top-ten/")
+    [system.diagnostics.process]::start("msedge","https://www.youtube.com/watch?v=dQw4w9WgXcQ/")
 }
 
 # kill a process by name
-Stop-Process -name iexplore
+##Stop-Process -name iexplore
 
 # kill a process by PID
-Stop-Process -id 3680
+##Stop-Process -id 3680
 
 # End
