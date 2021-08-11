3f8c2fb8c3e58d795bddf7f3e4fe065775c6282e
Jorsh <joshm581@gmail.com>
2021-08-11 13:37:10 -0700

    added domaindiggerV1

---
+++ ddigger.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+# Script Name:      Domain Digger
+# Author:           marburgja
+# Last Rev:         20210811
+# Purpose:          List Info On A Domain
+
+# Functions
+domaindigger(){
+    whois $domain
+    dig $domain
+    host $domain
+    nslookup $domain
+}
+
+# Main
+echo "Enter Domain To Query..."
+read domain
+domaindigger > /home/marburgja/Desktop/domaindigger.txt
+
+# End
