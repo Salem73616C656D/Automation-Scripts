afc5878096736d169d0fdb3fd69e78260a03d3f2
Jorsh <joshm581@gmail.com>
2021-08-27 14:50:37 -0700

    created python Hellow World script

---
+++ HWPython.py
@@ -0,0 +1,10 @@
+# Script Name:      Python: HW
+# Author:           marburgja
+# Last Rev:         20210828
+# Purpose:          Print "Hello World" Using Python
+
+# Variables
+msg = "Hello World"
+
+# Main
+print(msg)
