f4f10acf0b5e754da3323404d8bd95a5ac8ddf2e
Jorsh <joshm581@gmail.com>
2021-08-31 16:31:47 -0700

    added timestamp script

---
+++ timestamp.sh
@@ -0,0 +1,18 @@
+# Script Name:      TimeStamp
+# Author:           marburgja
+# Last Rev:         20210831
+# Purpose:          copies file to current directory
+
+# Variables
+date=$(date +"%T")
+
+# Main
+echo "Copying Syslog To Desktop $date..."
+cp -r /var/log/syslog /home/marburgja/Desktop
+echo "Switching To Desktop Directory $date..."
+cd /home/marburgja/Desktop/
+echo "Appending Date And Time To Filename $date..."
+mv syslog syslog"$(date +"%Y%m%d_%T")"
+echo "Copy Complete $date"
+
+# End
