2c21cbff28cf52ea60097e44809cb4dcdfcbd128
Jorsh <joshm581@gmail.com>
2021-08-31 16:33:05 -0700

    edited format

--- timestamp.sh
+++ timestamp.sh
@@ -9,10 +9,13 @@ date=$(date +"%T")
 # Main
 echo "Copying Syslog To Desktop $date..."
 cp -r /var/log/syslog /home/marburgja/Desktop
+
 echo "Switching To Desktop Directory $date..."
 cd /home/marburgja/Desktop/
+
 echo "Appending Date And Time To Filename $date..."
 mv syslog syslog"$(date +"%Y%m%d_%T")"
+
 echo "Copy Complete $date"
 
 # End
