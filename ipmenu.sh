c0a1254bd1e2096a8275ab9938f190886be8db09
Jorsh <joshm581@gmail.com>
2021-09-02 15:08:34 -0700

    corrected date

--- ipmenu.sh
+++ ipmenu.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Script Name:      ipmenu
 # Author:           marburgja
-# Last Rev:         20210901
+# Last Rev:         20210902
 # Purpose:          creates menu for various ip commands
 # Reference:        https://askubuntu.com/questions/1705/how-can-i-create-a-select-menu-in-a-shell-script
 
