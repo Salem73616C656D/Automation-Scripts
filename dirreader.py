3046f7bfbbfb51aa028934b1c8c84fcb57a5f5f1
Jorsh <joshm581@gmail.com>
2021-09-08 22:26:31 -0700

    created dirreader script

---
+++ dirreader.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+# Script Name:      dirreader
+# Author:           marburgja
+# Last Rev:         20210908
+# Purpose:          generates all directories, sub-directories, and files for a provide path
+
+# Libraries
+import os
+
+# Variables
+path=input("Please Enter Filepath:")
+
+# Functions
+def dirreader():
+    for (root, dirs, files) in os.walk(path):
+        print(root)
+
+        dirs[:] = [x for x in dirs if not x.startswith('.')]
+
+        for dir in dirs:
+            print(os.path.join(root, dir))
+
+        for file in files:
+            print(os.path.join(root, file))        
+
+# Main
+dirreader()
+
+# End
