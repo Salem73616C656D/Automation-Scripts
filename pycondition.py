925afb9c07b9707188bde148738511d87565b02a
Jorsh <joshm581@gmail.com>
2021-09-13 16:51:03 -0700

    created pycondition script

---
+++ pycondition.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+# Script Name:      py_if
+# Author:           marburgja
+# Last Rev:         2021090113
+# Purpose:          python if-statement tutorial
+
+# Variables
+
+a=input("'A' Value:")
+b=input("'B' Value:")
+
+# Main
+
+
+# List of Logical Conditionals
+
+# if a == b:
+#    print("A equals B")
+
+#if a != b:
+#    print("A does not equal B")
+
+#if a < b:
+#    print("A is less than B")
+
+#if a <= b:
+#    print("A is less than or equal to B")
+
+#if a > b:
+#    print("A is greater than B")
+
+#if a >= b:
+#    print("A is greater than or equal to B")
+
+
+
+# elif statement
+
+#if a == b:
+#    print("it's the same number")
+#elif a != b:
+#    print("they're not the same")
+
+
+
+# elif/else statement
+
+if a > b:
+    print("A is greater than B")
+elif a < b:
+    print("A is less than B")
+else:
+    print("A equals B")
+
+
+# End
