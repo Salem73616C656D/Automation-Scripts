2a0e155719fd3e24466cd962f97c97bac11e672e
Joshua Marburg <87879035+marburgja@users.noreply.github.com>
2021-11-16 10:19:11 -0800

    Update fileseek.py
    added comments

--- fileseek.py
+++ fileseek.py
@@ -13,14 +13,14 @@ def find_all():
     path=input("Enter Directory Path To Scan:")
     name=input("Enter Name To Scan For:")
     result=[]
-    for root, dirs, files in os.walk(path):
+    for root, dirs, files in os.walk(path): # searches dirs and sub dirs for "name"
         if name in files:
             result.append(os.path.join(root,name))
     if result==[]:
-        print("Error 404")
+        print("Error 404") # printed when there are no results from the scan
     else:
-        print(result)
-        print("Total Number Found:", len(result))
+        print(result) # prints search results
+        print("Total Number Found:", len(result)) # prints the number of results from search
 
 # Main
 find_all()
