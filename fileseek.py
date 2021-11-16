a9a86f8ab438cf5575c582248f9e43320b7752fa
Josh <joshm581@gmail.com>
2021-11-16 09:48:01 -0800

    created fileseek/directory analysis script

---
+++ fileseek.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# Script Name:      fileseek
+# Author:           marburgja
+# Last Rev:         20211115
+# Purpose:          Search a directory for certain files and print results to screen
+
+# Libraries
+import os
+# Variables
+
+# Functions
+def find_all():
+    path=input("Enter Directory Path To Scan:")
+    name=input("Enter Name To Scan For:")
+    result=[]
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            result.append(os.path.join(root,name))
+    if result==[]:
+        print("Error 404")
+    else:
+        print(result)
+        print("Total Number Found:", len(result))
+
+# Main
+find_all()
+# End
