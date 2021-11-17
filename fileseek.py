b7da01173b8f7f97e4fb400a50b38131af1bb612
marburgja <joshm581@gmail.com>
2021-11-17 09:17:47 -0800

    added comments/format changes

--- fileseek.py
+++ fileseek.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # Script Name:      fileseek
 # Author:           marburgja
-# Last Rev:         20211116
+# Last Rev:         20211117
 # Purpose:          Search a directory for certain files and print results to screen
 
 # Libraries
@@ -11,16 +11,15 @@ import os,time,datetime,hashlib
 # Variables
 
 # Functions
-def currentime():
+def currentime(): # creates a timestamp to be called 
     rn=datetime.datetime.now()
     return rn.strftime('%m-%d-%Y %H:%M:%S')
 
-def filesize(patharg):
+def filesize(patharg): # gets filesize of the selected file
     size=os.path.getsize(patharg)
     return size
     
-
-def hasher(patharg):
+def hasher(patharg): # creates a hash of the selected file
     h=hashlib.md5()
     with open(patharg,'rb') as file:
         chunk=0
@@ -35,11 +34,11 @@ def find_all():
     result={}
     for root, dirs, files in os.walk(path): # searches dirs and sub dirs for "name"
         print(root)
-        for dir in dirs:
+        for dir in dirs: # these for loops print all the directories and files that are being scanned
             print(os.path.join(root,dir))
         for file in files:
             print(os.path.join(root,file))
-        if name in files:
+        if name in files: # if the file is found, add it to the result dictionary
             pathtofile=os.path.join(root,name)
             fileresult=[currentime(), hasher(pathtofile),filesize(pathtofile) ]
             result[pathtofile]=fileresult
@@ -61,7 +60,6 @@ Format: [Time, Hash Value, File Size(Bytes)]
 """)
         print(result) # prints search results
         print("Total Number Found:", len(result)) # prints the number of results from search
-
 
 # Main
 
