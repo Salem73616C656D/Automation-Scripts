12a99c878d32fd7e9fb7160576a84d2dc31a09e3
marburgja <joshm581@gmail.com>
2021-11-16 18:11:29 -0800

    added recursive file analysis functionality challenge 32

--- fileseek.py
+++ fileseek.py
@@ -6,24 +6,45 @@
 
 # Libraries
 
-import os
+import os,time,datetime,hashlib
 
 # Variables
 
 # Functions
+def currentime():
+    rn=datetime.datetime.now()
+    return rn.strftime('%m-%d-%Y %H:%M:%S')
+
+def filesize(patharg):
+    size=os.path.getsize(patharg)
+    return size
+    
+
+def hasher(patharg):
+    h=hashlib.md5()
+    with open(patharg,'rb') as file:
+        chunk=0
+        while chunk != b'':
+            chunk=file.read(1024)
+            h.update(chunk)
+    return h.hexdigest()
 
 def find_all():
     path=input("Enter Directory Path To Scan:")
     name=input("Enter Name To Scan For:")
-    result=[]
+    result={}
     for root, dirs, files in os.walk(path): # searches dirs and sub dirs for "name"
         if name in files:
-            result.append(os.path.join(root,name))
-    if result==[]:
+            pathtofile=os.path.join(root,name)
+            fileresult=[currentime(), hasher(pathtofile),filesize(pathtofile) ]
+            result[pathtofile]=fileresult
+    if result=={}:
         print("Error 404") # printed when there are no results from the scan
     else:
+        print("Format: [Time, Hash Value, File Size(Bytes)]")
         print(result) # prints search results
         print("Total Number Found:", len(result)) # prints the number of results from search
+
 
 # Main
 
